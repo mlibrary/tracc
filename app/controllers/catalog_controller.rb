3193cbc75ab75349c2f7015957deda762c99a8b5
Blanco <blancoj@umich.edu>
2019-08-22 10:51:30 -0400

    make search work

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -178,8 +178,13 @@ class CatalogController < ApplicationController
     # solr request handler? The one set in config[:default_solr_parameters][:qt],
     # since we aren't specifying it otherwise.
 
-    config.add_search_field 'all_fields', label: 'All Fields'
-
+    #config.add_search_field 'all_fields', label: 'All Fields'
+    config.add_search_field('all_fields', label: 'All Fields') do |field|
+      all_names = config.show_fields.values.map(&:field).join(" ")
+      field.solr_parameters = {
+        qf: "#{all_names} file_format_tesim all_text_timv",
+      }
+    end
 
     # Now we see how to over-ride Solr request handler defaults, in this
     # case for a BL "search field", which is really a dismax aggregate
