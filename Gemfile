b14f9d7a8d3b0cc5e5933af5f2946ec400a65418
Nabeela Jaffer <njaffer@umich.edu>
2019-10-02 14:02:16 -0400

    refine charts

--- Gemfile
+++ Gemfile
@@ -80,3 +80,4 @@ gem 'devise-guests', '~> 0.6'
 gem 'blacklight-marc', '>= 7.0.0.rc1', '< 8'
 
 gem 'ettin', '~> 1.2'
+
--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,9 +1,12 @@
 class ReportsController < ApplicationController
 	protect_from_forgery with: :null_session
 
+    def show
+
+    end	
 	def generate
 # "cycle"=>"All", "status"=>"Any", "activity_type"=>"All", "chart"=>"Requests vs Cycle", "chart_type"=>"Bar",
 
-		byebug
+		
 	end	
 end
