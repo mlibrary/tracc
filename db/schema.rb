57ab9eab7a1c4e14631fc0c4d67e653a4a1ce793
Gordon Leacock <gordonl@umich.edu>
2019-12-03 16:53:12 -0500

    updated db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -244,7 +244,6 @@ ActiveRecord::Schema.define(version: 2019_12_16_184545) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
