69db88d41cd4f5eff2433115003d1cf3aeebbd53
gordonleacock <gordonl@umich.edu>
2020-01-29 19:59:18 -0500

    fixup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
 	def check_current_access
-		# access levels
-		# 0=view only; 1=view plus search; 2=view, search, modify cards; 3= super user/dev
+    # access levels
+    # 0=view only; 1=view plus search; 2=view, search, modify cards; 3= super user/dev
 
-		puts ">>>>> user_signed_in? is #{user_signed_in?}"
-	  puts ">>>>> current_user is #{current_user}" if user_signed_in?
+    puts ">>>>> user_signed_in? is #{user_signed_in?}"
+    puts ">>>>> current_user is #{current_user}" if user_signed_in?
 
 	  return 0 unless user_signed_in?
 
