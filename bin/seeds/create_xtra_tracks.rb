75047da913ba2c1fa46744c95acec1f0a2d3d261
Nabeela Jaffer <njaffer@umich.edu>
2020-04-08 17:09:48 -0400

    add project level track

---
+++ bin/seeds/create_xtra_tracks.rb
@@ -0,0 +1,11 @@
+#bundle exec rails runner bin/seeds/create_tracks.rb 
+Track.create(card_id:'1',epic_title: 'LSP',track: 'Project-Level', status: 0)
+Track.create(card_id:'2',epic_title: 'Catalog Search',track: 'Project-Level', status: 0)
+Track.create(card_id:'3',epic_title: 'Library Search',track: 'Project-Level', status: 0)
+Track.create(card_id:'3',epic_title: 'Library Search',track: 'Project-Level', status: 0)
+Track.create(card_id:'4',epic_title: 'Deep Blue',track: 'Project-Level', status: 0)
+Track.create(card_id:'5',epic_title: 'Digital Collections Platform',track: 'Project-Level', status: 0)
+Track.create(card_id:'6',epic_title: 'Fulcrum',track: 'Project-Level', status: 0)
+Track.create(card_id:'7',epic_title: 'Website Redesign',track: 'Project-Level', status: 0)
+Track.create(card_id:'8',epic_title: 'Hathi Trust',track: 'Project-Level', status: 0)
+
