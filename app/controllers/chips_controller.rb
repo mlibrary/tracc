bb0e7622c90c0d3a83c91c8fde2c60ac0f29574e
Nabeela Jaffer <njaffer@umich.edu>
2020-04-14 17:43:23 -0400

    Add chips edit option

--- app/controllers/chips_controller.rb
+++ app/controllers/chips_controller.rb
@@ -1,12 +1,37 @@
 class ChipsController < ApplicationController
-
+protect_from_forgery with: :null_session	
 	def resources
 	end
 	
-	def save_resources
+	def save_chips
+     
+     id = params["id"]
+     str = "id='"+id+"'"
+     
+     if (id.empty?)
+	   one_chip = Chip.new      	
+     else
+       one_chip = Chip.where(str)
+       one_chip = one_chip.first
+     end  
+     
+       
+	   
+     if (!one_chip.nil?) 
+       if (params["uniqname"].empty?)
+       	one_chip.destroy!
+       else
+        one_chip.uniqname = params["uniqname"]
+        one_chip.fname = params["fname"]
+        one_chip.lname= params["lname"]
+        one_chip.dept = params["dept"]
+        one_chip.project = params["project"]
+        one_chip.ops= params["ops"]
+        one_chip.save!
+      end
 	end
-
-	def edit
+end 
+	def staff_chips
 	end
 		
 end
