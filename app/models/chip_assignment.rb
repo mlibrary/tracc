5eb2657e0f111cbea4f66df5be43caeefe1f1516
Nabeela Jaffer <njaffer@umich.edu>
2020-04-15 16:57:54 -0400

    add export all

--- app/models/chip_assignment.rb
+++ app/models/chip_assignment.rb
@@ -1,2 +1,12 @@
 class ChipAssignment < ApplicationRecord
+	def self.to_csv
+  
+    CSV.generate do |csv|
+      csv << column_names
+      all.each do |result|
+        csv << result.attributes.values_at(*column_names)
+      end
+    end
+  end
+  
 end
