1dc7da2a0400ed1ac1783df65a757da218161a12
Nabeela Jaffer <njaffer@umich.edu>
2020-05-12 18:42:47 -0400

    fix roundup bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,7 +31,10 @@ module ApplicationHelper
                        parts = fte_rounded.to_s.split(".")
                        if (parts.count>1)
                          result = parts[1].to_i
-                       if (result> 0 && result < 10)
+                       if (parts[1].length == 1)  
+                          result = result * 10
+                       end 
+                       if (result>= 0 && result < 10)
                         fte_rounded = parts[0].to_i 
                        elsif (result> 10 && result < 40)
                         fte_rounded = parts[0].to_i + 0.25 
