0ac08ed2bcfe15ada059395387fe0d892bd4b94c
Nabeela Jaffer <njaffer@umich.edu>
2020-05-20 11:54:35 -0400

    cleanup dashboards months

--- app/controllers/cycle_controller.rb
+++ app/controllers/cycle_controller.rb
@@ -1,2 +1,21 @@
 class CycleController < ApplicationController
+
+	def new
+		@cname = Cycle.all
+		
+	end	
+
+	def save
+		cur_cycle = Cycle.where("current_cycle =1") 
+		cur_cycle.each do |one|
+			one.current_cycle = 0 
+			one.save!
+		end
+	    str = "cycle_name='" + params["cycle"]+ "'" 
+		new_cycle = Cycle.where(str)
+		new_cycle.each do |one|
+			one.current_cycle = 1 
+			one.save!
+		end
+	end	
 end
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,37 @@
 module DashboardHelper
+	def get_months
+        
+nameofmonths = ['','January', 'February','March','April','May','June','July','August', 'September','October','November','December']
+
+cur_cycle = Cycle.where("current_cycle =1")
+cycle = cur_cycle.first.cycle_name
+s = cur_cycle.first.start.month
+stmonth = ""
+endmonth = ""
+
+
+if (s != nil)
+  stmonth = nameofmonths[s]
+  if s<=8
+    s = s +3
+  else
+    s = s+3 -12 
+  end  
+  endmonth = nameofmonths[s]
+end 
+
+s = cur_cycle.first.start.month
+
+months = ['','','','','']
+i = 1
+while (i<=4) do
+  months[i] = nameofmonths[s]
+  i = i + 1
+  s = s+1 
+  if (s>12)
+    s= 1
+  end
+end 
+		return months
+	end	
 end
