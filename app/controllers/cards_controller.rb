8fa2474354d64ec3d623b0e3603c82680b730dc6
Nabeela Jaffer <njaffer@umich.edu>
2020-10-28 14:09:29 -0400

    sort by cycle on all cards page

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -7,7 +7,7 @@ class CardsController < ApplicationController
   # GET /cards.json
   def index
     if (approve_access)
-      @cards = Card.all.order(:in_cycle)(:title)
+      @cards = Card.all.order(:in_cycle)
     else
       flash.now[:notice] = I18n.t('blacklight.no_access')
     end
